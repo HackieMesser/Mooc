9f2f648b027770485bac37ce4606f32eba4f37ab
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-02-19 22:43:13 +0100

    Update 5c.py

--- 5c.py
+++ 5c.py
@@ -3,3 +3,14 @@ def times_ten(start_index, end_index):
     for i in range(start_index, end_index+1):
         dic[i]=i*10
     return dic
+
+def factorials(n):
+    facs={}
+    list=[]
+    for i in range(1,n+1):
+        y=1
+        for x in range(1,i+1):
+            b=x*y
+            y=b
+        facs[i]=y
+    return facs
