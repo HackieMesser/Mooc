98329ad7832babab8cc7808376fb8f48cf6de5b3
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-02-23 23:01:39 +0100

    Update 5d.py

--- 5d.py
+++ 5d.py
@@ -10,3 +10,16 @@ def create_tuple(x,y,z):
 #alt
 def create_tuple(x,y,z):
     return (min([x,y,z]),max([x,y,z]),sum([x,y,z]))
+
+#02
+def oldest_person(people):
+    index=0
+    for i in people:
+        if index==0:
+            b=i[1]
+        elif i[1]<b:
+            b=i[1]
+        index+=1
+    for c in people:
+        if c[1]==b:
+            return c[0]
