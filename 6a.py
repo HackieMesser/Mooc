63df4673d769a072916357a946207bfd6c38b3d9
HackieMesser <41595086+HackieMesser@users.noreply.github.com>
2023-03-06 21:18:35 +0100

    Update 6a.py

--- 6a.py
+++ 6a.py
@@ -1,1 +1,14 @@
+def largest():
+
+    with open("numbers.txt") as num:
+        index=0
+        for line in num:
+            if index==0:
+                larg=line
+            elif larg<line:
+                larg=line
+            index+=1
+            print(larg)
+        return int(larg) 
+
 
